11231319b59d04093735670a41ebba7f4055bfa4
Wemerson <wemersonqueiroz@yahoo.com>
2022-04-09 06:51:49 +0100

    Updating App with Data Mapping

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React from "react"
 import "./App.css"
+import Navbar from "./components/Navbar"
+import Card from "./components/Card"
+import data from "./components/data"
 
 function App() {
-  return <div className="App"></div>
+  const places = data.map(place => {
+    return <Card key={place.title} place={place} />
+  })
+  return (
+    <div className="App">
+      <Navbar />
+      {places}
+    </div>
+  )
 }
 
 export default App

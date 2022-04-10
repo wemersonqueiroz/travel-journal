ad17ebf6674abbca1acc311717322197c856d0a9
Wemerson <wemersonqueiroz@yahoo.com>
2022-04-10 06:24:14 +0100

    edited database for testing

--- src/components/data.js
+++ src/components/data.js
@@ -1,13 +1,25 @@
 const data = [
   {
-    title: "Mount Fuji",
-    location: "Japan",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    title: "London Eye",
+    location: "London",
+    googleMapsUrl: "https://goo.gl/maps/oKdwy8RujHA5MsoR6",
     startDate: "12 Jan, 2021",
     endDate: "24 Jan, 2021",
     description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+      "The London Eye, or the Millennium Wheel, is a cantilevered observation wheel on the South Bank of the River Thames in London. It is Europe's tallest cantilevered observation wheel, and is the most popular paid tourist attraction in the United Kingdom with over 3 million visitors annually.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1545853332-147d5073187e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+  },
+  {
+    title: "London Eye",
+    location: "London",
+    googleMapsUrl: "https://goo.gl/maps/oKdwy8RujHA5MsoR6",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description:
+      "The London Eye, or the Millennium Wheel, is a cantilevered observation wheel on the South Bank of the River Thames in London. It is Europe's tallest cantilevered observation wheel, and is the most popular paid tourist attraction in the United Kingdom with over 3 million visitors annually.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1545853332-147d5073187e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
   },
 ]
 

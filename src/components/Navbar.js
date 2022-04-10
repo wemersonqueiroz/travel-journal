cfce340e58f21513fdec385c5f3a33e4af3f2a81
Wemerson <wemersonqueiroz@yahoo.com>
2022-04-10 06:18:59 +0100

    added globe png to navbar as react component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,19 @@
 import React from "react"
 
+import ImageComponent from "./Images"
+import "../index.css"
+
 export default function Navbar() {
   return (
     <div>
-      <navbar>Wemerson's Travel Journal</navbar>
+      <navbar>
+        <ImageComponent
+          source={"globe.png"}
+          alt="Globe Icon"
+          className="globe"
+        />{" "}
+        Wemerson's Travel Journal
+      </navbar>
     </div>
   )
 }

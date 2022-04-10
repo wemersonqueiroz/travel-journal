cca4233f11637e0b1ce8446841b23051b800755b
Wemerson <wemersonqueiroz@yahoo.com>
2022-04-10 06:20:15 +0100

    updated details and added local images as react components

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,36 @@
 import React from "react"
+import ImageComponent from "./Images"
+import "../index.css"
 
 export default function Card(props) {
   return (
-    <div className="card place--card">
-      <img
-        src={props.place.imageUrl}
-        className="place--img"
-        alt={props.place.location}
-      />
-      <div>
-        <h6>{props.place.location}</h6>
-        <a href={props.place.googleMapsUrl}> View on Google Maps</a>
-        <h2>{props.title}</h2>
-        <h6>
-          {props.place.startDate} - {props.place.endDate}
-        </h6>
-        <text>{props.place.description}</text>
+    <>
+      <div className="place--card">
+        <img
+          src={props.place.imageUrl}
+          className="place--img"
+          alt={props.place.location}
+        />
+        <div clasName="place--text--box">
+          <div className="place--location-box">
+            <ImageComponent
+              source={"pin.png"}
+              alt="Location Pin"
+              className="place--pin"
+            />
+            <h6 className="location">{props.place.location}</h6>
+            <a href={props.place.googleMapsUrl} className="google--maps">
+              View on Google Maps
+            </a>
+          </div>
+          <h2 className="title">{props.place.title}</h2>
+          <h6 className="period">
+            {props.place.startDate} - {props.place.endDate}
+          </h6>
+          <text className="description">{props.place.description}</text>
+        </div>
       </div>
       <hr />
-    </div>
+    </>
   )
 }
